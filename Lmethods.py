be963b6eaa809b690d26fcd362c62f03be16829b
epicMonkey13 <56354271+epicMonkey13@users.noreply.github.com>
2022-09-16 11:41:37 +0100

    added self-practice

--- Lmethods.py
+++ Lmethods.py
@@ -104,6 +104,38 @@
 #
 # print(x)
 
+#exercises from w3schools
+
+# fruits = ["apple", "banana", "cherry"]
+# print(fruits[1])
+
+#Change the value from "apple" to "kiwi", in the fruits list.
+# fruits = ["apple", "banana", "cherry"]
+# fruits[0] = "kiwi"
+# print(fruits)
+
+#Use the append method to add "orange" to the fruits list.
+# fruits = ["apple", "banana", "cherry"]
+# fruits.append("orange")
+# print(fruits)
+
+#Use the insert method to add "lemon" as the second item in the fruits list.
+# fruits = ["apple", "banana", "cherry"]
+# fruits.insert(1, "lemon")
+# print(fruits)
+
+#Use the remove method to remove "banana" from the fruits list.
+# fruits = ["apple", "banana", "cherry"]
+# fruits.remove("banana")
+# print(fruits)
+
+#Use negative indexing to print the last item in the list.
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+
+
+
 
 
 
